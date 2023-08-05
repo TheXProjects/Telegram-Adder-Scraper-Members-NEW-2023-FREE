8ac1d8637005195d27e9b71e48a35b1c84e60f7e
XProjects <TheXProjects2022@gmail.com>
2023-08-05 16:39:16 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 # enter your API credentials below - generate at https://my.telegram.org/auth
-api_id = 123456789
-api_hash = "46sadasd8asd46a8sa4d6ad48"
+api_id = 26488237
+api_hash = "9c3d35ae97bed6762027da17cbec5b33"
 
 #THIS IS JUST A GENERIC API_ID AND API_HASH!
 
